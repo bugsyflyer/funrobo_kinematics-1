8c7c643ece485be9c84fd7ca5516f25c4c6827d9
bugsyflyer <bodonnell@olin.edu>
2026-04-02 00:08:16 -0400

    quintic and trapezoid implementations

--- examples/traj_gen.py
+++ examples/traj_gen.py
@@ -88,7 +88,7 @@ class CubicPolynomial():
 
 class QuinticPolynomial():
     """
-    Cubic interpolation with position and velocity boundary constraints.
+    quintic interpolation with position and velocity boundary constraints.
     """
 
     def __init__(self, ndof=None):
@@ -98,9 +98,9 @@ class QuinticPolynomial():
         self.ndof = ndof
 
     
-    def solve(self, q0, qf, qd0, qdf, qdd0, qddf, T):
+    def solve(self, q0, qf, qd0, qdf, T):
         """
-        Compute cubic polynomial coefficients for each DOF.
+        Compute quintic polynomial coefficients for each DOF.
 
         Parameters
         ----------
@@ -124,15 +124,15 @@ class QuinticPolynomial():
         qf = np.asarray(qf, dtype=float)
         qd0 = np.zeros_like(q0) if qd0 is None else np.asarray(qd0, dtype=float)
         qdf = np.zeros_like(q0) if qdf is None else np.asarray(qdf, dtype=float)
-        qdd0 = np.zeros_like(q0) if qdd0 is None else np.asarray(qdd0, dtype=float)
-        qddf = np.zeros_like(q0) if qddf is None else np.asarray(qddf, dtype=float)
+        qdd0 = np.zeros_like(q0)
+        qddf = np.zeros_like(q0)
         
         A = np.array(
                 [[1, t0, t0**2, t0**3, t0**4, t0**5],
                  [0, 1, 2*t0, 3*t0**2, 4*t0**3, 5*t0**4],
                  [0, 0, 2, 6*t0, 12*t0**2, 20*t0**3],
                  [1, tf, tf**2, tf**3, tf**4, tf**5],
-                 [0, 1, 2*tf, 3*tf**2, 4*tf**3, 5*tf**4]
+                 [0, 1, 2*tf, 3*tf**2, 4*tf**3, 5*tf**4],
                  [0, 0, 2, 6*tf, 12*tf**2, 20*tf**3],
                 ])
 
@@ -174,11 +174,100 @@ class QuinticPolynomial():
             X[i, 2, :] = qdd    # acceleration
 
         return t, X
+    
+class Trapezoid():
+    """
+    Trapezoid interpolation with position and velocity boundary constraints.
+    """
+
+    def __init__(self, ndof=None):
+        """
+        Initialize the trajectory generator.
+        """
+        self.ndof = ndof
+
+    
+    def solve(self, q0, qf, qd0, qdf, T, V=None):
+        """
+        Compute trapeziod for each DOF.
+
+        Parameters
+        ----------
+        q0 : array-like, shape (ndof,)
+            Initial positions.
+        qf : array-like, shape (ndof,)
+            Final positions.
+        T : float
+            Total trajectory duration.
+        """
+        t0, tf = 0, T
+        q0 = np.asarray(q0, dtype=float)
+        qf = np.asarray(qf, dtype=float)
+
+        self.q0 = q0
+        self.qf = qf
+        self.tf = tf
+
+        dq = qf - q0
+
+        #arbitrary v if none put in
+        if V is None:
+            V = 1.5 * dq / tf
+        self.V = V
+
+        self.tb = (q0 - qf + V*tf)/V #blend time
+        self.alpha = V/self.tb #acc
+        
+
+    def generate(self, t0=0, tf=0, nsteps=100):
+        """
+        Generate position, velocity, and acceleration trajectories.
+
+        Parameters
+        ----------
+        t0 : float
+            Start time.
+        tf : float
+            End time.
+        nsteps : int
+            Number of time samples.
+        """
+        #prob add a check for - sign
+        t = np.linspace(t0, tf, nsteps)
+        X = np.zeros((self.ndof, 3, len(t)))
+
+        for i in range(self.ndof): # iterate through all DOFs
+            q0 = self.q0[i]
+            qf = self.qf[i]
+            V = self.V[i]
+            tb = self.tb[i]
+            alpha = self.alpha[i]
+            tf = self.tf
+
+            for j, t_j in enumerate(t):
+                #acc phase
+                if t_j < tb:
+                    q = q0 + 0.5 * alpha * t_j**2
+                    qd = alpha * t_j
+                    qdd = alpha
+                elif t_j <= (tf-tb):
+                    q = 0.5*(qf+q0-(V*tf)) + (V*t_j)
+                    qd = V
+                    qdd = 0
+                elif t_j <= tf:
+                    q = qf - 0.5*alpha*tf**2 + alpha*tf*t_j - 0.5*alpha*t_j**2
+                    qd = alpha*(tf - t_j)
+                    qdd = -alpha
+                X[i, 0, :] = q      # position
+                X[i, 1, :] = qd     # velocity
+                X[i, 2, :] = qdd    # acceleration
+
+        return t, X
 
 
 def main():
     ndof = 2
-    method = CubicPolynomial(ndof=ndof)
+    method = QuinticPolynomial(ndof=ndof)
     mode = "joint"
 
     # --------------------------------------------------------
